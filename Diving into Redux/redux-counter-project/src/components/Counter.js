faca949f21355dc0047daeda8a2131ba8d6a9d5f
Akash More <akashkmore1@gmail.com>
2022-03-24 22:26:27 +0530

    Learn adding action payloads in redux

--- Diving into Redux/redux-counter-project/src/components/Counter.js
+++ Diving into Redux/redux-counter-project/src/components/Counter.js
@@ -21,6 +21,10 @@ const Counter = () => {
     dispatch({ type: "INCREMENT" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 4 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
   };
@@ -33,6 +37,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 4</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
