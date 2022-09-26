a7d046a865899ec50e08e0658b9c0278700670fc
Akash More <akashkmore1@gmail.com>
2022-09-26 16:08:48 +0530

    Add header component

--- 01-Basics_Of_React_Revise/src/App.js
+++ 01-Basics_Of_React_Revise/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import "./App.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      <section className="body__section"> Body </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ 01-Basics_Of_React_Revise/src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return <section className="header__section">CRUD User Name and AGE</section>;
+};
+
+export default Header;
