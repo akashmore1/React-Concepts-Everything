857912bf77534d379ff07ae0f2bfdd0a00c5ebda
Akash More <akashkmore1@gmail.com>
2022-09-27 05:11:11 +0530

    Make header responsive

--- 01-Basics_Of_React_Revise/src/components/Header/Header.js
+++ 01-Basics_Of_React_Revise/src/components/Header/Header.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <section className="header__section">CRUD User Name and AGE</section>;
+  return (
+    <>
+      <section className="header__section display_wide">
+        CRUD User Name and AGE
+      </section>
+      <section className="header__section display_short">CRUD</section>
+    </>
+  );
 };
 
 export default Header;
